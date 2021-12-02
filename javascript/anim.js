c9cf43e506347527588c089f691d859bf4a4bcef
FratiaXd <welcomcherry@gmail.com>
2021-12-02 16:20:14 +0000

    commit

--- javascript/anim.js
+++ javascript/anim.js
@@ -1,3 +1,21 @@
+//Navigation bar animation
+function line_animate() {
+    var line = document.getElementById("line");
+    var width = 0;
+    var int = setInterval(frame, 5);
+    function frame() {
+        if (width == 98) {
+            clearInterval(int);
+        } else {
+            width++; 
+            line.style.width = width + '%'; 
+        }
+    }
+}
+//Zoom product image
+
+
+//Video loop
 const videoLocations = ["../assets/video1.webm", "../assets/video2.webm", "../assets/video3.webm", "../assets/video4.webm"]
 var videoCount = videoLocations.length;
 var i = 0;
