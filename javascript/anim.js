6ee2f65f6a4bcd2a9231c1887abdc72e857751c4
FratiaXd <welcomcherry@gmail.com>
2021-12-15 19:54:18 +0000

    js

--- javascript/anim.js
+++ javascript/anim.js
@@ -16,10 +16,69 @@ function line_animate() {
 //Navigation bar for mobile devices
 function sidebarFun() {
     var x = document.getElementById("navigation");
-    x.classList.toggle('active')
+    var y = document.getElementById("increaseHead");
+    if (localStorage.fontSize == 'normal'){
+        x.classList.toggle('active');
+    }
+    else {
+        y.classList.toggle('active');
+    }
 }
 
 //Color and font settings
+let increase = document.getElementById("font_incr");
+let deflt = document.getElementById("font_default");
+let nav = document.getElementById("navigation");
+let grey = document.getElementById("grey");
+let beige = document.getElementById("beige");
+let white = document.getElementById("white");
+let headerBack = document.getElementById("headnav");
+
+increase.addEventListener('click', increaseFont);
+deflt.addEventListener('click', normalFont);
+grey.addEventListener('click', makeGrey);
+beige.addEventListener('click', makeBeige);
+white.addEventListener('click', makeWhite);
+
+function increaseFont() {
+    nav.setAttribute('id', 'increaseHead');
+    localStorage.fontSize = 'large'; 
+};
+if (localStorage.fontSize == 'large'){
+    nav.setAttribute('id', 'increaseHead');
+};
+
+function normalFont() {
+    nav.setAttribute('id', 'navigation');
+    localStorage.fontSize = 'normal';
+};
+if (localStorage.fontSize == 'normal'){
+    nav.setAttribute('id', 'navigation');
+};
+
+function makeGrey() {
+    headerBack.setAttribute('id', 'greyHead');
+    localStorage.color = 'grey';
+};
+if (localStorage.color == 'grey'){
+    headerBack.setAttribute('id', 'greyHead');
+};
+
+function makeBeige() {
+    headerBack.setAttribute('id', 'beigeHead');
+    localStorage.color = 'beige';
+};
+if (localStorage.color == 'beige'){
+    headerBack.setAttribute('id', 'beigeHead');
+};
+
+function makeWhite() {
+    headerBack.setAttribute('id', 'whiteHead');
+    localStorage.color = 'white';
+};
+if (localStorage.color == 'white'){
+    headerBack.setAttribute('id', 'whiteHead');
+};
 
 //Zoom product image
 function onClick(element) {
